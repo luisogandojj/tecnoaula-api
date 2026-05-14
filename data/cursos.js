cc4fbec3a980167516f9a31dbde5069c945f9718
luisogandojj <luisogandorodriguezjj@gmail.com>
2026-05-14 13:15:25 +0100

    feat: initialize cursos resource with data, controller, routing, and server setup

---
+++ data/cursos.js
@@ -0,0 +1,12 @@
+const cursos = [
+    {
+        id: 1,
+        nombre: "Informática básica",
+        descripcion: "Curso de iniciación al uso del ordenador e Internet",
+        duracion: "30 horas",
+        nivel: "Inicial",
+        precio: 120
+    }
+];
+
+module.exports = cursos;
